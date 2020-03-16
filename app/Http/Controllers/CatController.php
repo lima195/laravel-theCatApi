fa353ab14f1d585e7831b89c7d1986b43a10d38d
Pedro Lima <phgdl.19@gmail.com>
2020-03-16 18:02:10 -0300

    Add ixudra/curl and add option params to breads request

--- app/Http/Controllers/CatController.php
+++ app/Http/Controllers/CatController.php
@@ -3,67 +3,66 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Ixudra\Curl\Facades\Curl;
+use App\Http\Requests\ListCatsRequest;
 
 class CatController extends Controller
 {
 	const CAT_API = "https://api.thecatapi.com/v1/";
 	const BREEDS_ENDPOINT = "breeds/";
-	private $catToken = '';
 
-    public function __construct()
-    {
-        $this->middleware('auth:api');
+	const DEFAULT_PAGE = 0;
+	const DEFAULT_LIMIT = 20;
 
-        $user = auth()->user();
-        $this->catToken = $user->cat_token;
-    }
+	private $_catToken = '';
 
-    public function list(Request $request)
-    {
-    	$curl = curl_init();
+	public function __construct()
+	{
+		$this->middleware('auth:api');
 
-    	curl_setopt_array($curl, array(
-    	  CURLOPT_URL => self::CAT_API . self::BREEDS_ENDPOINT,
-    	  CURLOPT_RETURNTRANSFER => true,
-    	  CURLOPT_ENCODING => "",
-    	  CURLOPT_MAXREDIRS => 10,
-    	  CURLOPT_TIMEOUT => 30,
-    	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    	  CURLOPT_CUSTOMREQUEST => "GET",
-    	  CURLOPT_HTTPHEADER => array(
-    	    "x-api-key: {$this->catToken}"
-    	  ),
-    	));
+		$user = auth()->user();
+		$this->_catToken = $user->cat_token;
+	}
 
-    	$response = curl_exec($curl);
-    	$err = curl_error($curl);
+	public function list(Request $request)
+	{
+		$attach_breed 	= ($request->attach_breed ? $request->attach_breed : 0);
+		$page 			= ($request->page ? $request->page : self::DEFAULT_PAGE);
+		$limit 			= ($request->limit ? $request->limit : self::DEFAULT_LIMIT);
 
-    	curl_close($curl);
+		$response = Curl::to(self::CAT_API . self::BREEDS_ENDPOINT)
+						->withData([
+							'attach_breed' => $attach_breed,
+							'page' => $page,
+							'limit' => $limit,
+						])
+						->withHeader("x-api-key: {$this->_catToken}")
+						->asJson()
+						->get();
 
-    	if ($err) {
-    	  	return response()->json(['error' => $err, 401]);
-    	} else {
-    		$data = json_decode($response);
-    	  	return response()->json(['data' => $data, 200]);
-    	}
-    }
+		if(isset($response->status) && isset($response->message)) {
+			return response()->json(['error' => $response->message], $response->status);
+		} else {
+			return response()->json(['data' => $response], 200);
+		}
+	}
 
-    public function setCatToken(Request $request)
-    {
-    	$catToken = $request->cat_token;
+	public function setCatToken(Request $request)
+	{
+		$catToken = $request->cat_token;
 
-    	if($catToken) {
-    		try {
+		if($catToken) {
+			try {
 				$user = auth()->user();
 				$user->cat_token = $catToken;
 				$user->save();
-    		} catch (\Exception $e) {
-    			return response()->json(['message' => $e], 401);
-    		}
+			} catch (\Exception $e) {
+				return response()->json(['message' => $e], 401);
+			}
 
-    		return response()->json(['message' => "Success!"]);
-    	} else {
-    		return response()->json(['message' => "Token not found!", 401]);
-    	}
-    }
+			return response()->json(['message' => "Success!"]);
+		} else {
+			return response()->json(['message' => "Token not found!", 401]);
+		}
+	}
 }
